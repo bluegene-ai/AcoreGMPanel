cfac35c9bc1c9edbf4b04848e853942e7a32a776
bluegene-ai <bluegene@foxmail.com>
2026-04-22 11:37:01 +0800

    为所有角色和账号增加跳转。

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bluegene-ai/acore-gm-panel',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c440c172888554b62203889b4b8b507b34590fc7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'bluegene-ai/acore-gm-panel' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c440c172888554b62203889b4b8b507b34590fc7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'maxmind-db/reader' => array(
+            'pretty_version' => 'v1.13.1',
+            'version' => '1.13.1.0',
+            'reference' => '2194f58d0f024ce923e685cdf92af3daf9951908',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../maxmind-db/reader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
